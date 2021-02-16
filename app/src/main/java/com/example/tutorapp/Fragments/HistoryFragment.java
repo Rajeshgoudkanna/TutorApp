36fbdcf4620a3205106f676f5d6030ff860bc3f1
Shaileshkrishna <krishnashailesh5@gmail.com>
2021-02-16 00:06:00 -0500

    HistoryFragment

---
+++ app/src/main/java/com/example/tutorapp/Fragments/HistoryFragment.java
@@ -0,0 +1,80 @@
+package com.example.tutorapp.Fragments;
+
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.GridView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.example.tutorapp.Activities.Utils;
+import com.example.tutorapp.Adapters.HistoryAdapter;
+import com.example.tutorapp.R;
+import com.example.tutorapp.model.Course;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HistoryFragment extends Fragment {
+    GridView gridview;
+    List<Course> a1;
+    SharedPreferences sharedPreferences;
+    String session;
+    HistoryAdapter historyAdapter;
+    View view;
+
+    public static HistoryFragment historyFragment() {
+        HistoryFragment fragment = new HistoryFragment();
+        return fragment;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        view = inflater.inflate(R.layout.fragment_history, container, false);
+        ((AppCompatActivity)getActivity()).getSupportActionBar().setTitle("History");
+        sharedPreferences =getActivity().getSharedPreferences(Utils.SHREF, Context.MODE_PRIVATE);
+        session = sharedPreferences.getString("user_name", "def-val");
+        a1= new ArrayList<>();
+        gridview=(GridView)view.findViewById(R.id.gridview);
+        Query query = FirebaseDatabase.getInstance().getReference("Enrolled Courses").child(session);
+        query.addListenerForSingleValueEvent(valueEventListener);
+        return view;
+    }
+    ValueEventListener valueEventListener = new ValueEventListener() {
+        @Override
+        public void onDataChange(DataSnapshot dataSnapshot) {
+            a1.clear();
+            if (dataSnapshot.exists()) {
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    Course homeDataPojo = snapshot.getValue(Course.class);
+                    a1.add(homeDataPojo);
+                }
+                historyAdapter = new HistoryAdapter(a1,getActivity());
+                gridview.setAdapter(historyAdapter);
+            }
+            else {
+                Toast.makeText(getContext(), "No data Found", Toast.LENGTH_SHORT).show();
+            }
+        }
+        @Override
+        public void onCancelled(DatabaseError databaseError) {
+
+        }
+    };
+
+}

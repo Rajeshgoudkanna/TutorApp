a17b6684690d983769b624bf40c4c77104fca27d
sonam <sonam24ahuja@gmail.com>
2021-03-01 23:37:33 -0500

    registration and splash screen

--- app/src/main/java/com/example/tutorapp/SplashScreen.java
+++ app/src/main/java/com/example/tutorapp/SplashScreen.java
@@ -1,7 +1,5 @@
 package com.example.tutorapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
@@ -10,21 +8,22 @@ import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class SplashScreen extends AppCompatActivity {
 
-    Animation top_anim, btm_anim;
+    Animation top_anim,btm_anim;
     ImageView imageView;
-    TextView textlogo, texttagline;
-
+    TextView textlogo,texttagline;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-        imageView = findViewById(R.id.imageView);
-        textlogo = findViewById(R.id.textView4);
-        texttagline = findViewById(R.id.textView5);
-        top_anim = AnimationUtils.loadAnimation(this, R.anim.top_animation);
-        btm_anim = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
+        imageView= findViewById(R.id.imageView);
+        textlogo= findViewById(R.id.textView4);
+        texttagline=findViewById(R.id.textView5);
+        top_anim= AnimationUtils.loadAnimation(this, R.anim.top_animation);
+        btm_anim= AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
         imageView.setAnimation(top_anim);
         textlogo.setAnimation(btm_anim);
         texttagline.setAnimation(btm_anim);
@@ -34,6 +33,6 @@ public class SplashScreen extends AppCompatActivity {
                 Intent i = new Intent(SplashScreen.this, login.class);
                 startActivity(i);
             }
-        }, 3000);
+        },3000);
     }
 }
